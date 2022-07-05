f5009cd6fd4d716de2662cf32b4bd90cd7d38240
Ruzell11 <johnruzell123@gmail.com>
2022-07-05 19:40:54 +0800

    deploy

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
+let http = new XMLHttpRequest();
 
-search = () =>{
-    
-    const searchBox = document.getElementById('search-item').value.toUpperCase();
-    const storeItems = document.getElementById('product-list');
-    const product = document.querySelectorAll('.product');
-    const productName = storeItems.getElementsByTagName("h1");
+http.open('get' , 'pokemon.json' ,true);
 
-    for(var i = 0 ; i < productName.length; i++){
-        let match = product[i].getElementsByTagName("h1")[0];
-        console.log('sd')
-        if(match){
-            let textValue = match.textContent  || match.innerHTML
 
-            if(textValue.toUpperCase().indexOf(searchBox) > -1){
-                product[i].style.display = "";
-            }else{
-                product[i].style.display = "none";
-            }
+http.send();
+
+http.onload = function(){
+    if(this.readyState == 4 && this.status == 200){
+        let product = JSON.parse(this.responseText);
+        let output = "";
+        
+       console.log(product)
+
+        for(let item of product){
+            output += `
+            
+            <div class="products  shadow mx-5">
+            <div class="pokemon">
+            <div class="img-container">
+            <img src = ${item.image} alt="${item.image}" >
+            </div>
+            <div class="details d-flex justify-content-between align-items-center p-4">
+            <p class="title">${item.title}</p>
+            <p class='description'>${item.ability}</p>
+            <p class='price'>${item.type}</p>
+            </div>
+            </div>
+            </div>
+            
+            `;
+           
         }
+        document.getElementById('pokemonContainer').innerHTML = output
     }
-    
+   
 }
+
+
